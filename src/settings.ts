ce4fea7003413df96f42eeb02fafa3c46606381c
biixin <113357477+biixin@users.noreply.github.com>
2026-01-29 00:51:41 -0300

    Add files via upload

---
+++ src/settings.ts
@@ -0,0 +1,27 @@
+export const SETTINGS = {
+  name: 'Amanda',
+  age: 24,
+  distance: {
+    min: 3,
+    max: 12,
+  },
+  status: 'Procurando algo casual',
+  onlineText: 'Online agora',
+  availableText: 'Disponível agora',
+  introMessage: 'está muito perto de você!',
+  redirectingText: 'Redirecionando...',
+  connectingText: 'Conectando você com',
+  messages: [
+    'Oi, vi seu perfil',
+    'Oi, tudo bem?',
+    'Olá, podemos conversar?',
+    'Oi, vamos conversar?',
+  ],
+  fotos: [
+    'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=800',
+    'https://images.pexels.com/photos/1181690/pexels-photo-1181690.jpeg?auto=compress&cs=tinysrgb&w=800',
+    'https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg?auto=compress&cs=tinysrgb&w=800',
+    'https://images.pexels.com/photos/1181391/pexels-photo-1181391.jpeg?auto=compress&cs=tinysrgb&w=800',
+    'https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=800',
+  ],
+};
